87c525d947173668c9e7de3d2ff5d5aad65a7e1c
Faizan Khan <mfkhan1994@gmail.com>
2019-05-09 15:16:35 +0200

    Some changes

--- mask_rcnn.py
+++ mask_rcnn.py
@@ -3,9 +3,6 @@ import sys
 import random
 import math
 import numpy as np
-import skimage.io
-import matplotlib
-import matplotlib.pyplot as plt
 import cv2
 import time
 
@@ -17,10 +14,6 @@ from mrcnn import utils
 import mrcnn.model as modellib
 from mrcnn import visualize
 from mrcnn.config import Config
-
-
-global modelmskrcnn
-modelmskrcnn = None
 
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
@@ -61,38 +54,41 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
                'teddy bear', 'hair drier', 'toothbrush']
 
-def filter_class_ids(r):
+class MaskRcnn:
+    modelmskrcnn = None
 
-    rr = dict()
-    rr['class_ids'] = np.empty((0,),np.int32)
-    rr['scores'] = np.empty((0,))
-    rr['rois'] = np.empty((0,r['rois'].shape[1]))
-    rr['masks'] = np.empty((r['masks'].shape[0], r['masks'].shape[1], 0))
+    @staticmethod
+    def filter_class_ids(r):
 
-    for idx, id in enumerate(r['class_ids']):
-        if class_names[id] in ['person', 'car']:
-            rr['class_ids'] = np.hstack((rr['class_ids'], id))
-            rr['scores'] = np.hstack((rr['scores'],r['scores'][idx]))
-            rr['rois'] = np.vstack((rr['rois'], r['rois'][idx, :]))
-            rr['masks'] = np.append(rr['masks'], np.expand_dims(r['masks'][:, :, idx],axis=-1),axis=-1)
-    return rr
+        rr = dict()
+        rr['class_ids'] = np.empty((0,),np.int32)
+        rr['scores'] = np.empty((0,))
+        rr['rois'] = np.empty((0,r['rois'].shape[1]))
+        rr['masks'] = np.empty((r['masks'].shape[0], r['masks'].shape[1], 0))
 
-def masked_frame(batch_frames):
-    global modelmskrcnn
-    if modelmskrcnn is None:
-        # Create model object in inference mode.
-        modelmskrcnn = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
-        # Load weights trained on MS-COCO
-        modelmskrcnn.load_weights(COCO_MODEL_PATH, by_name=True)
+        for idx, id in enumerate(r['class_ids']):
+            if class_names[id] in ['person', 'car']:
+                rr['class_ids'] = np.hstack((rr['class_ids'], id))
+                rr['scores'] = np.hstack((rr['scores'],r['scores'][idx]))
+                rr['rois'] = np.vstack((rr['rois'], r['rois'][idx, :]))
+                rr['masks'] = np.append(rr['masks'], np.expand_dims(r['masks'][:, :, idx],axis=-1),axis=-1)
+        return rr
 
-    results = modelmskrcnn.detect(batch_frames, verbose=0)
-    results = [filter_class_ids(res) for res in results]
+    @staticmethod
+    def get_colored_frame(batch_frames):
+        if MaskRcnn.modelmskrcnn is None:
+            # Create model object in inference mode.
+            MaskRcnn.modelmskrcnn = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+            # Load weights trained on MS-COCO
+            MaskRcnn.modelmskrcnn.load_weights(COCO_MODEL_PATH, by_name=True)
 
-    res = results[0]
-    res_frames = []
-    for res, frame in zip(results, batch_frames):
-        res_frame = visualize.display_instances(frame, res['rois'], res['masks'], res['class_ids'],
-                    class_names, res['scores'])
-        res_frames.append(res_frame)
-    return res_frames
+        results = MaskRcnn.modelmskrcnn.detect(batch_frames, verbose=0)
+        results = [MaskRcnn.filter_class_ids(res) for res in results]
+
+        res_frames = []
+        for res, frame in zip(results, batch_frames):
+            res_frame = visualize.display_instances(frame, res['rois'], res['masks'], res['class_ids'],
+                        class_names, res['scores'])
+            res_frames.append(res_frame)
+        return res_frames
 
